d3f1a051f070a7adaeef8341c03cdbbf2710b2f7
Lillian Lutzner <lillian.lutzner@gmail.com>
2025-10-26 19:30:52 -0300

    developing the restaurant activation option

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def nome_programa():
 def opcoes():
     print("1. Cadastrar restaurante")
     print("2. Listar restaurante")
-    print("3. Ativar restaurante")
+    print("3. Alterar status do restaurante")
     print("4. Sair\n")
 
 def cadastrar_restaurante():
@@ -50,6 +50,27 @@ def listar_restaurantes():
 
     voltar_ao_menu_principal()
 
+def alternar_status_restaurante():
+        print("Alternar status do restaurante")
+        nome_restaurante = input("Digite o nome do restaurante cujo status você quer alterar: ")
+        restaurante_encontrado = False
+
+        for restaurante in restaurantes:
+            if nome_restaurante == restaurante['nome']:
+                restaurante_encontrado = True
+                print(f"Restaurante {nome_restaurante} encontrado. Alterando status...")
+                restaurante['ativo'] = not restaurante['ativo']
+                
+                mensagem = f"O restaurante {nome_restaurante} foi ativado com sucesso!" if restaurante["ativo"] else f"O restaurante {nome_restaurante} foi desativado com sucesso!"
+                print(mensagem)
+
+        if not restaurante_encontrado:
+            print("Restaurante não encontrado!")
+        
+
+
+        voltar_ao_menu_principal()
+
 def encerrar():
     os.system('cls')
     print("Encerrando o programa")
@@ -66,7 +87,7 @@ def escolher_opcao():
         elif numero_menu == 2:
             listar_restaurantes()
         elif numero_menu == 3:
-            print("Ativar restaurante")
+            alternar_status_restaurante()
         elif numero_menu == 4:
             encerrar()
         else:
